d1bb5694b857ba08feaf79e47147250aabb88ec0
TheSusort <k.susort@stud.uis.no>
2014-02-03 00:36:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
 			padding: 0;
 			background: #000000;
 			text-align: center;
-
+	
 		}
-
+	
 		td {
 			padding: 0 0 10px;
 			text-align: center;
@@ -18,7 +18,7 @@
 			color: #FFFFFF;
 		}
 		table {
-
+	
 			width: 100%;
 		}
 		img {
@@ -62,34 +62,35 @@
 <td>
 
 	<p> This is the directory script </p>
-	 <?php
 
-$images = "upload/"; # Location of small versions
-$big    = ""; # Location of big versions (assumed to be a subdir of above)
-$cols   = 3; # Number of columns to display
+<?php
 
-if ($handle = opendir($images)) {
-   while (false !== ($file = readdir($handle))) {
-       if ($file != "." && $file != ".." && $file != rtrim($big,"/")) {
-           $files[] = $file;
-       }
-   }
-   closedir($handle);
-}
-
-$colCtr = 0;
-
-echo '<table width="100%" cellspacing="3"><tr>';
-
-foreach($files as $file)
-{
-  if($colCtr %$cols == 0)
-	echo '</tr><tr><td colspan="'.$cols.'"><hr /></td></tr><tr>';
-	echo '<td align="center"><a href="' . $images . $big . $file . '"><img src="' . $images . $file . ' "width="200px" length="auto" /></a></td>';
-	$colCtr++;
-}
-
-echo '</table>' . "\r\n";
+	$images = "upload/"; # Location of small versions
+	$big    = ""; # Location of big versions (assumed to be a subdir of above)
+	$cols   = 3; # Number of columns to display
+	
+	if ($handle = opendir($images)) {
+	   while (false !== ($file = readdir($handle))) {
+	       if ($file != "." && $file != ".." && $file != rtrim($big,"/")) {
+	           $files[] = $file;
+	       }
+	   }
+	   closedir($handle);
+	}
+	
+	$colCtr = 0;
+	
+	echo '<table width="100%" cellspacing="3"><tr>';
+	
+	foreach($files as $file)
+	{
+	  if($colCtr %$cols == 0)
+		echo '</tr><tr><td colspan="'.$cols.'"><hr /></td></tr><tr>';
+		echo '<td align="center"><a href="' . $images . $big . $file . '"><img src="' . $images . $file . ' "width="200px" length="auto" /></a></td>';
+		$colCtr++;
+	}
+	
+	echo '</table>' . "\r\n";
 
 ?> 
 </td>
