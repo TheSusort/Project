1db4b2035fe037a84c2605861e45cd919d1ca6f1
TheSusort <k.susort@stud.uis.no>
2014-02-03 00:08:47 +0100

    Nattfix
    Klarte ikke å legge dette fra meg i kveld. Fant en bra måte å displaye
    bildene på.  Si fra hva dere synes.

--- index.php
+++ index.php
@@ -2,6 +2,39 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <title> Gruppe F's fantastiske løsning til å laste opp filer </title>
+	<style>
+		body {
+			margin: 0 auto 20px;
+			padding: 0;
+			background: #000000;
+			text-align: center;
+
+		}
+
+		td {
+			padding: 0 0 10px;
+			text-align: center;
+			font: 9px sans-serif;
+			color: #FFFFFF;
+		}
+		table {
+
+			width: 100%;
+		}
+		img {
+			display: block;
+			margin: 20px auto 10px;
+			max-width: 900px;
+			outline: none;
+			
+		}
+		img:active {
+			max-width: 100%;
+		}
+		a:focus {
+			outline: none;
+		}
+	</style>
 </head>
 
 <body>
@@ -29,12 +62,36 @@
 <td>
 
 	<p> This is the directory script </p>
-	<?php $folder = 'upload/'; $filetype = '*.*'; 
-	$files = glob($folder.$filetype); echo '<table>'; 
-	for ($i=0; $i<count($files); $i++) { echo '<tr><td>'; 
-	echo '<a name="'.$i.'" href="#'.$i.'"><img src="'.$files[$i].'"height=25%" width="25%" /></a>'; 
-	echo substr($files[$i],strlen($folder),strpos($files[$i], '.')-strlen($folder)); 
-	echo '</td></tr>'; } echo '</table>'; ?>
+	 <?php
+
+$images = "upload/"; # Location of small versions
+$big    = ""; # Location of big versions (assumed to be a subdir of above)
+$cols   = 3; # Number of columns to display
+
+if ($handle = opendir($images)) {
+   while (false !== ($file = readdir($handle))) {
+       if ($file != "." && $file != ".." && $file != rtrim($big,"/")) {
+           $files[] = $file;
+       }
+   }
+   closedir($handle);
+}
+
+$colCtr = 0;
+
+echo '<table width="100%" cellspacing="3"><tr>';
+
+foreach($files as $file)
+{
+  if($colCtr %$cols == 0)
+	echo '</tr><tr><td colspan="'.$cols.'"><hr /></td></tr><tr>';
+	echo '<td align="center"><a href="' . $images . $big . $file . '"><img src="' . $images . $file . ' "width="200px" length="auto" /></a></td>';
+	$colCtr++;
+}
+
+echo '</table>' . "\r\n";
+
+?> 
 </td>
 </tr>
 
