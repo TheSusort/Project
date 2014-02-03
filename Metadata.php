1ae4bbe74828e4d71a318a391e896fc8c98d162e
brt2 <berntsvenning@gmail.com>
2014-02-03 16:27:55 +0100

    La til funksjoner.

--- Metadata.php
+++ Metadata.php
@@ -13,11 +13,53 @@
 			$data[$teller][0] = "Legg til filnavn her";
 			$data[$teller][1] = "Legg til karakter her";
 			$data[$teller][2] = "Legg til tagg her";
-			$data[$teller][3] = "Legg til komentarer her";
+			$data[$teller][3] = "Legg til kommentarer her";
 			
 			$teller++;
-		}
-		
+		}		
 		return;
-	}	
+	}
+	
+	function HentKarakter($filnavn)
+	{
+		$teller = 0;
+		
+		while (true)
+		{
+			if ($filnavn == $data[$teller][0])
+			{
+				return $data[$teller][1];
+			}			
+			$teller++;
+		}
+	}
+	
+	function HentTagg($filnavn)
+	{
+		$teller = 0;
+		
+		while (true)
+		{
+			if ($filnavn == $data[$teller][0])
+			{
+				return $data[$teller][2];
+			}			
+			$teller++;
+		}
+	}
+	
+	function HentKommentar($filnavn)
+	{
+		$teller = 0;
+		
+		while (true)
+		{
+			if ($filnavn == $data[$teller][0])
+			{
+				return $data[$teller][3];
+			}			
+			$teller++;
+		}
+	}
+	
 ?>
