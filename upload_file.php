e885010d23cdfcd4922c7cdc1eaca9f623f53cf1
TheSusort <k.susort@stud.uis.no>
2014-02-03 11:23:29 +0100

    kommentarer + redirect
    Har lagt på kommentarer på upload, så blir det litt lettere å finne
    fram. også redirect til index etter 3 sekund.

--- upload_file.php
+++ upload_file.php
@@ -1,5 +1,7 @@
 <?php
-$allowedExts = array("gif", "jpeg", "jpg", "png");
+// skjekker om filen er et bilde, nå skal det ikke bety om det er store eller små bokstaver
+// i fil-extentionen. 
+$allowedExts = array("gif", "jpeg", "jpg", "png",  "GIF", "JPEG", "JPG", "PNG");
 $temp = explode(".", $_FILES["file"]["name"]);
 $extension = end($temp);
 if ((($_FILES["file"]["type"] == "image/gif")
@@ -8,26 +10,31 @@ if ((($_FILES["file"]["type"] == "image/gif")
 || ($_FILES["file"]["type"] == "image/pjpeg")
 || ($_FILES["file"]["type"] == "image/x-png")
 || ($_FILES["file"]["type"] == "image/png"))
+// 20 MB maks størrelse
 && ($_FILES["file"]["size"] < 20000000)
 && in_array($extension, $allowedExts))
   {
+  // Sjekker for feil, gir ut feilmelding hvis.
   if ($_FILES["file"]["error"] > 0)
     {
     echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
     }
   else
     {
+	// Returnerer info om bilde som ble lastet opp. 
     echo "Upload: " . $_FILES["file"]["name"] . "<br>";
     echo "Type: " . $_FILES["file"]["type"] . "<br>";
     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
-
+	
+	//sjekker om filen eksisterer fra før av.
     if (file_exists("upload/" . $_FILES["file"]["name"]))
       {
       echo $_FILES["file"]["name"] . " already exists. ";
       }
     else
       {
+	  // her lagres bilde i upload mappen, og gir en konfirmasjon om dette.
       move_uploaded_file($_FILES["file"]["tmp_name"],
       "upload/" . $_FILES["file"]["name"]);
       echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
@@ -38,4 +45,6 @@ else
   {
   echo "Invalid file";
   }
+  // Denne setningen gjør at man returnerer automatisk til indexsiden.
+  echo '<meta http-equiv="refresh" content="3;URL=index.php" />'; 
 ?>
