0b7e842ff0a9412f8add60b1508871bfb36ae2e0
gluckzu <gluckzu@gmail.com>
2014-02-08 08:10:23 +0100

    Bildeviser.php: get_File_List, get_img_list, check_for_new_img
    get_File_List($big, $images):
    return array of files names from dir.
    get_img_list($dir):
    return array of .jpg and .bmp files names from dir.
    check_for_new_img($dir):
    check for image som are not registered in database and add tem to the
    db.

--- Bildeviser.php
+++ Bildeviser.php
@@ -8,28 +8,76 @@
 		$images = "Bilder/";
 		$big    = "";
 		$cols   = 3;
-		
-		if ($handle = opendir($images)) {
-		   while (false !== ($file = readdir($handle))) {
-			   if ($file != "." && $file != ".." && $file != rtrim($big,"/")) {
-				   $files[] = $file;
-			   }
-		   }
-		   closedir($handle);
-		}
+
+//        $files = get_File_List($big, $images);
+        $files = get_img_list("Bilder");
 
 		$colCtr = 0;
+
+        check_for_new_img('Bilder');
 
 		echo '<table width="100%" cellspacing="3"><tr>';
 
 		foreach($files as $file)
 		{
 		  if($colCtr %$cols == 0)
-			echo '</tr><tr><td colspan="'.$cols.'"><hr /></td></tr><tr>';
-			echo '<td align="center"><a href="' . $images . $big . $file . '"><img src="' . $images . $file . ' "width="200px" length="auto" /></a></td>';
+			echo '
+			        </tr>
+			        <tr>
+			            <td colspan="'.$cols.'">
+			                <hr />
+			            </td>
+			        </tr>
+			        <tr>';
+			echo '
+			        <td align="center">
+			            <a href="' . $images . $big . $file . '">
+			                <img src="' . $images . $file . ' "width="200px" length="auto" />
+			            </a>
+			        </td>';
 			$colCtr++;
 		}
 
 		echo '</table>' . "\r\n";
 	}
+
+// return array of files names from dir.
+    function get_File_List($big, $images)
+    {
+        if ($handle = opendir($images)) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file != "." && $file != ".." && $file != rtrim($big,"/")) {
+                    $files[] = $file;
+                }
+            }
+            closedir($handle);
+        }
+        return $files;
+    }
+
+// return array of .jpg and .bmp files names from dir.
+    function get_img_list($dir)
+    {
+        $f = scandir($dir);
+
+        foreach ($f as $file){
+            if(preg_match('/\.jpg/i', $file) || preg_match('/\.png/i', $file)){
+                $files[] = $file;
+            }
+        }
+        return $files;
+    }
+
+// check for image som are not registered in database and add tem to the db.
+    function check_for_new_img($dir)
+    {
+        $files_on_disc  = get_img_list($dir);
+        $files_in_db    = db_select('file_liste', 'filename');
+        $result = array_diff($files_on_disc, $files_in_db);
+        foreach($result as $rslt)
+        {
+            db_insert('file_liste', 'filename', $rslt);
+        }
+    }
+
 ?>
